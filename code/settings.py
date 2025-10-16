8bdb266e050708c2492a5f448c705c74591b5bba
Harsh Vardhan <hv21082008@gmail.com>
2025-10-17 01:10:54 +0530

    Update settings.py

--- code/settings.py
+++ code/settings.py
@@ -1,1 +1,11 @@
+# code/settings.py
+SCREEN_W = 1280
+SCREEN_H = 720
+FPS = 60
 
+# Map size (larger than screen)
+MAP_W = 1920
+MAP_H = 1080
+
+# Paths
+ASSETS_DIR = "../assets"  # run main.py from code/ folder
